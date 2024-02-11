7f53ab4219fac6770263f0366e4212d70f456e69
atrox3d <atrox3d@gmail.com>
2024-02-11 08:29:38 +0100

    memoized traveler pythonic

--- gridtraveler.py
+++ gridtraveler.py
@@ -1,26 +1,33 @@
 def gridtraveler(m: int, n: int, memo={}) -> int:
-    if memo is not None:
+    try:
         key = (m, n)
         if key in memo:
             return memo[key]
+    except TypeError:
+        pass
 
     if m == n == 1: return 1
     if 0 in (m, n): return 0
 
     ret = gridtraveler(m-1, n, memo) + gridtraveler(m, n-1, memo)
-    if memo is not None:
-        memo[key] = ret
     
+    try:
+        memo[key] = ret
+    except TypeError:
+        pass
+
     return ret
 
-memo = {}
-print(gridtraveler(1, 1, memo))
+print(gridtraveler(1, 1))
 
-memo = {}
-print(gridtraveler(2, 3, memo))
+print(gridtraveler(2, 3))
 
 memo = {}
 print(gridtraveler(3, 3, memo))
+print(memo)
 
 memo = {}
 print(gridtraveler(18, 18, memo))
+
+memo = {}
+print(gridtraveler(100, 100, memo))
