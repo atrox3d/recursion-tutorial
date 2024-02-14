95537401029f9686eaac46556b97a9cdff818dd5
atrox3d <atrox3d@gmail.com>
2024-02-14 08:25:00 +0100

    canconstruct

---
+++ canconstruct.py
@@ -0,0 +1,12 @@
+def canconstruct(target: str, wordbank: list[str], memo: dict=None) -> bool:
+    '''
+    write a function canconstruct(target, wordbank) that accepts 
+    a target string and an array of strings as arguments
+
+    the function should return a boolelan indicating wether
+    or not target can be constructed by concatenating elements of
+    the wordbank array
+
+    you may reuse elements of wordbank as many times as needed
+    '''
+    
