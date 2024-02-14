7427bebc12f37d695e8c735b9f589738f9010fe3
atrox3d <atrox3d@gmail.com>
2024-02-14 11:03:56 +0100

    canconstruct

--- canconstruct.py
+++ canconstruct.py
@@ -9,4 +9,36 @@ def canconstruct(target: str, wordbank: list[str], memo: dict=None) -> bool:
 
     you may reuse elements of wordbank as many times as needed
     '''
-    
+    memo = {} if memo is None else memo
+    try:
+        return memo[target]
+    except: pass
+    if target == '': return True
+
+    for word in wordbank:
+        if target.startswith(word):
+            result = canconstruct(target[len(word):], wordbank, memo)
+            try:
+                memo[target] = result
+            except: pass
+
+            if result == True:
+                return True
+    return False
+
+data = (
+    ('abcdef', 'ab abc cd def abcd'.split()),
+    ('skateboard', 'bo rd ate t ska sk boar'.split()),
+    ('enterapotentpot', 'a p ent enter ot o t'.split()),
+    ('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', 
+     'e ee eee eeee eeeee eeeeee'.split()),
+)
+
+for target, wordbank in data:
+    outer_memo = {}
+    print(f'{target = }')
+    print(f'{wordbank = }')
+    result = canconstruct(target, wordbank, outer_memo)
+    print(f'{result = }')
+    print(f'{outer_memo = }')
+    print()
