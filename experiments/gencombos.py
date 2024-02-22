51b9995e632a2e2ba5351c92352e1654c4978e8b
atrox3d <atrox3d@gmail.com>
2024-02-22 17:24:42 +0100

    checking solutions

--- experiments/gencombos.py
+++ experiments/gencombos.py
@@ -1,5 +1,9 @@
 
 def gencombos( target, column ):
+    '''
+    https://stackoverflow.com/a/70342171
+    does not work well
+    '''
     if column == 1:
         yield [target]
     else:
@@ -11,5 +15,21 @@ def gencombos( target, column ):
             for row in gencombos( target-i*column, column-1 ):
                 yield row+[i]
 
-for row in gencombos( 7, 3 ):
-    print(row)
+# for row in gencombos( 7, 3 ):
+    # print(row)
+
+def f(sum, k):
+    '''
+    good.
+    https://stackoverflow.com/a/74454054
+    '''
+    if k < 1:
+        return []
+    if k == 1:
+        return [(sum,)]
+    if k == 2:
+        return [(i,sum-i) for i in range(1, sum-k+2)]
+    
+    return [tup[:-1] + ab for tup in f(sum, k-1) for ab in f(tup[-1], 2)]
+
+print(f(10, 2))
