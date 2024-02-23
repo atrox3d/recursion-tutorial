cfef282a445044074426da09de944b15d397fc16
atrox3d <atrox3d@gmail.com>
2024-02-23 08:18:20 +0100

    log helper

--- experiments/combinatorics_for.py
+++ experiments/combinatorics_for.py
@@ -7,11 +7,17 @@ INDENTSTEP = 2
 INDENTCHAR = '.'
 INDENT = None
 
+LOGGERMETHOD = logger.debug
+
 def getindent():
     global INDENTLEVEL, INDENT, INDENTSTEP
     INDENTLEVEL = 0 if INDENTLEVEL is None else INDENTLEVEL+INDENTSTEP
     INDENT = INDENTLEVEL * INDENTCHAR
     return INDENT
+
+def log(message, loggermethod=LOGGERMETHOD):
+    message = f'{INDENT}{message}'
+    loggermethod(message)
 
 def f(sum, k):
     '''
@@ -20,31 +26,31 @@ def f(sum, k):
     '''
     INDENT = getindent()
     if k < 1:
-        logger.debug(f'{INDENT}BASE CASE: {sum=}, {k=}')
-        logger.debug(f'{INDENT}BASE CASE: {k} < 1, return []')
+        log(f'BASE CASE: {sum=}, {k=}')
+        log(f'BASE CASE: {k} < 1, return []')
         return []
     if k == 1:
-        logger.debug(f'{INDENT}BASE CASE: {sum=}, {k=}')
-        logger.debug(f'{INDENT}BASE CASE: k == {k} , return [({sum=},)]')
+        log(f'BASE CASE: {sum=}, {k=}')
+        log(f'BASE CASE: k == {k} , return [({sum=},)]')
         return [(sum,)]
     if k == 2:
-        logger.debug(f'{INDENT}BASE CASE: {sum=}, {k=}')
-        logger.debug(f'{INDENT}BASE CASE: k == {k}')
-        logger.debug(f'{INDENT}BASE CASE: return [(i,{sum}-i) for i in range(1, {sum}-{k}+2)]')
+        log(f'BASE CASE: {sum=}, {k=}')
+        log(f'BASE CASE: k == {k}')
+        log(f'BASE CASE: return [(i,{sum}-i) for i in range(1, {sum}-{k}+2)]')
         # ret = [(i,sum-i) for i in range(1, sum-k+2)]
         ret = []
         for i in range(1, sum-k+2):
             combo = (i, sum-i)
             ret.append(combo)
-        logger.debug(f'{INDENT}BASE CASE: return {ret}')
+        log(f'BASE CASE: return {ret}')
         return ret
     
-    logger.debug(f'{INDENT}RECURSIVE CASE: {sum=}, {k=}')
+    log(f'RECURSIVE CASE: {sum=}, {k=}')
     # ret = [tup[:-1] + ab for tup in f(sum, k-1) for ab in f(tup[-1], 2)]
     ret = []
-    logger.debug(f'{INDENT}RECURSIVE CASE: tups = f(sum={sum}, k=({k}-1))')
+    log(f'RECURSIVE CASE: tups = f(sum={sum}, k=({k}-1))')
     tups = f(sum, k-1)
-    logger.debug(f'{INDENT}RECURSIVE CASE: {tups=}')
+    log(f'RECURSIVE CASE: {tups=}')
     for tup in tups:
         abs = f(tup[-1], 2)
         for ab in abs:
