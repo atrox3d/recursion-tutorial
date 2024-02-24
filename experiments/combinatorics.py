d09fe0fcc6c7afeef98045769b3f57dc162c139f
atrox3d <atrox3d@gmail.com>
2024-02-24 08:30:15 +0100

    remove logging, include 0 as start

--- experiments/combinatorics.py
+++ experiments/combinatorics.py
@@ -2,30 +2,23 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def f(sum, k):
+def f(sum, k, start=0):
     '''
     good.
     https://stackoverflow.com/a/74454054
     '''
-    logger.debug(f'ENTER: {sum=}, {k=}')
-    if k < 1:
-        logger.debug(f'{k=} < 1, return []')
+    if k < start:
         return []
-    if k == 1:
-        logger.debug(f'k == {k} , return [({sum=},)]')
+    if k == start:
         return [(sum,)]
     if k == 2:
-        logger.debug(f'k == {k}')
-        logger.debug(f'return [(i,{sum}-i) for i in range(1, {sum}-{k}+2)]')
-        ret = [(i,sum-i) for i in range(1, sum-k+2)]
-        logger.debug(f'return {ret}')
+        ret = [(i,sum-i) for i in range(start, sum-k+2)]
         return ret
     
-    logger.debug('RECURSIVE CASE')
     return [tup[:-1] + ab 
             for tup in f(sum, k-1) 
             for ab in f(tup[-1], 2)]
 
 if __name__ == '__main__':
     logging.basicConfig(level='DEBUG', format='%(message)s')
-    print(f(5, 3))
+    print(f(5, 4))
