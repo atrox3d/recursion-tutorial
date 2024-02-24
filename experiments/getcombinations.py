de9a8c0ac5da948a18cfc31d3fe6c9ffb2095f90
atrox3d <atrox3d@gmail.com>
2024-02-24 13:34:24 +0100

    and to if

--- experiments/getcombinations.py
+++ experiments/getcombinations.py
@@ -5,22 +5,23 @@ def get_combinations(target, num_elements, combinations=None, start=0):
     # Calculate the sum of the current list of numbers
     combinations_sum = sum(combinations)
     # Check if the target is reached -> No further recursion necessary
-    if (combinations_sum == target and len(combinations) == num_elements):
-        # Return this combination of numbers
-        yield tuple(combinations)
+    if combinations_sum == target:
+        if len(combinations) == num_elements:
+            # Return this combination of numbers
+            yield tuple(combinations)
     else:
         # The combination of numbers doesn't yet total to target value
         # Iterate over each number from 1 to the target value 
         for number in range(start, target + 1):
             # Check that neither the target value nor number of elements will be exceeded
-            if (combinations_sum + number <= target 
-                and len(combinations) < num_elements):
-                # Add the number to the list
-                new_combo = combinations + [number]
-                # Find all solutions for the list
-                temp = list(get_combinations(target, num_elements, new_combo))
-                for c in temp:
-                    yield c
+            if combinations_sum + number <= target:
+                if len(combinations) < num_elements:
+                    # Add the number to the list
+                    new_combo = combinations + [number]
+                    # Find all solutions for the list
+                    temp = list(get_combinations(target, num_elements, new_combo))
+                    for c in temp:
+                        yield c
 
 if __name__ == '__main__':
     from pathlib import Path
