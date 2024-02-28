a27b8b7ce2c9d701deb9dd6bbea2eb16484564cb
atrox3d <atrox3d@gmail.com>
2024-02-28 09:55:45 +0100

    boom

--- experiments/mistures.py
+++ experiments/mistures.py
@@ -18,16 +18,20 @@ def mixtures_list(n: int, total: int):
     else:
         start = 0
 
+    combos = []
     for i in range(start, total + 1):
         left = total - i
         if n - 1:
             for y in mixtures(n - 1, left):
-                yield [i] + y
+                combos.append([i] + y)
         else:
-            yield [i]
+            combos.append([i])
+    return combos
 
 
 if __name__ == '__main__':
-    mix = list(mixtures(4, 3))
-    print(mix)
-    print(len(mix))
+    mix1 = list(mixtures(4, 3))
+    mix2 = mixtures_list(4, 3)
+    # print(mix)
+    # print(len(mix))
+    assert mix1 == mix2
