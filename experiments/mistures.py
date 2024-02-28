d0d9b6919242ff1b3fff45431b9e0d236f075677
atrox3d <atrox3d@gmail.com>
2024-02-28 09:53:03 +0100

    try converting to list

--- experiments/mistures.py
+++ experiments/mistures.py
@@ -12,6 +12,20 @@ def mixtures(n: int, total: int):
         else:
             yield [i]
 
+def mixtures_list(n: int, total: int):
+    if n == 1:
+        start = total
+    else:
+        start = 0
+
+    for i in range(start, total + 1):
+        left = total - i
+        if n - 1:
+            for y in mixtures(n - 1, left):
+                yield [i] + y
+        else:
+            yield [i]
+
 
 if __name__ == '__main__':
     mix = list(mixtures(4, 3))
