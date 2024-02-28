1d8006b5dd5b8396d96411d6ad7565d0f3646d0f
atrox3d <atrox3d@gmail.com>
2024-02-28 10:07:54 +0100

    fix5

--- experiments/mistures.py
+++ experiments/mistures.py
@@ -1,13 +1,13 @@
-def mixtures(n: int, total: int):
-    if n == 1:
+def mixtures(total: int, columns: int):
+    if columns == 1:
         start = total
     else:
         start = 0
 
     for i in range(start, total + 1):
         left = total - i
-        if n - 1:
-            for y in mixtures(n - 1, left):
+        if columns - 1:
+            for y in mixtures(left, columns - 1):
                 yield [i] + y
         else:
             yield [i]
@@ -30,9 +30,9 @@ def mixtures_list(total: int, columns: int):
 
 
 if __name__ == '__main__':
-    mix1 = list(mixtures(3, 4))
+    mix1 = list(mixtures(4, 3))
     mix2 = mixtures_list(4, 3)
-    print(mix2)
-    print(len(mix2))
+    # print(mix2)
+    # print(len(mix2))
     assert mix1 == mix2
     print('ok')
