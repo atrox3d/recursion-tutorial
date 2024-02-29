9cf7a85138cb412497f6bc3f2c2eafa00dce2092
atrox3d <atrox3d@gmail.com>
2024-02-29 13:52:27 +0100

    permute working version

---
+++ neetcode/permutations.py
@@ -0,0 +1,17 @@
+def permute(nums: list[int]) -> list[list[int]]:
+    result = []
+    # base case
+    if len(nums) == 1:
+        return [nums[:]]
+    
+    for value in range(len(nums)):
+        current = nums.pop(0)
+        perms = permute(nums)
+
+        for perm in perms:
+            perm.append(current)
+        result.extend(perms)
+        nums.append(current)
+    return result
+
+print(permute([1, 2, 3]))
