3c362b42817af49119fdca673220f1dd294c31bf
atrox3d <atrox3d@gmail.com>
2024-03-02 21:54:52 +0100

    first loop

---
+++ experiments/mixing.py
@@ -0,0 +1,10 @@
+total = 10
+
+for r1 in range(total+1):
+    # if total <= 0: break
+    print(f'{r1, total = }')
+    total -= 1
+    # for r2 in range(total):
+    #     if total <= 0: break
+    #     total -= 1
+    #     print(f'{r2, total = }')
