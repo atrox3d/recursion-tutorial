c7725c41aeaa5470425c996f651bc2d44fd55bd6
atrox3d <atrox3d@gmail.com>
2024-03-04 06:26:23 +0100

    include 0 qty in range

--- experiments/recipes.py
+++ experiments/recipes.py
@@ -10,17 +10,17 @@ def possible_recipes5(names, total_amount):
                     assert amount1+amount2+amount3+amount4+amount5 == total_amount
                     yield zip(names, [amount1, amount2, amount3, amount4, amount5])
                     
-def possible_recipes4(total_amount):
+def possible_recipes4(total_amount, start=1):
     amounts = []
-    for amount1 in range(1, total_amount+1):
+    for amount1 in range(start, total_amount+1):
         rest1 = total_amount - amount1
-        for amount2 in range(1, rest1+1):
+        for amount2 in range(start, rest1+1):
             rest2 = rest1 - amount2
-            for amount3 in range(1, rest2+1):
+            for amount3 in range(start, rest2+1):
                 amount4 = rest2 - amount3
                 assert amount1+amount2+amount3+amount4 == total_amount
                 amounts.append((amount1, amount2, amount3, amount4))
     return amounts
 
-amounts = possible_recipes4(5)
+amounts = possible_recipes4(5, start=0)
 print(amounts)
