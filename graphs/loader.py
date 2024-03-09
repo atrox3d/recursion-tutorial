0a314c84c169e7f2f19b5ec09d014ac0668f9bde
atrox3d <atrox3d@gmail.com>
2024-03-09 07:39:11 +0100

    wrong strategy

--- graphs/loader.py
+++ graphs/loader.py
@@ -28,6 +28,8 @@ class TxtGraph:
         self.edges = edges
         self._setup_edges()
         self.data = {}
+        self.rows = 0
+        self.cols = 0
 
     def _setup_edges(self):
         (   
@@ -36,18 +38,38 @@ class TxtGraph:
             self.UP, self.DOWN
         ) = list(self.edges)
     
-    def load(self):
-        with open(str(self.path), 'r') as file:
-            lines = [line.strip() for line in file.readlines()]
-        # print(ascii_lowercase)
-        ROWS = len(lines)
-        COLS = len(lines[0])
+    def _get_valid_coords(self, r, c):
+        coords = [  (r, c) for r, c in 
+                    [(r+y, c+x) for y in range(-1, 2) for x in range(-1, 2) if abs(y)!=abs(x)]
+                    if 0 <= r <= self.rows and 0 <= c <= self.cols
+                ]
+        return coords
+
+    def _parse(self, lines):
+        self.rows = len(lines)
+        self.cols = len(lines[0])
         for r, row in enumerate(lines):
             for c, char in enumerate(row):
                 if char in ascii_lowercase:
-                    self.data[char] = (r, c)
-                    
+                    self.data[char] = {'coords': (r, c), 'neighbours':[]}
+                    neighbours = self.data[char]['neighbours']
+                    for y, x in self._get_valid_coords(r, c):
+                        edge = lines[y][x]
+                        if edge in self.edges:
+                            match edge:
+                                case self.LEFT:
+                                    neighbours.append[lines[y][x-1]]
+                                case self.RIGHT:
+                                    pass
+                                case self.UP:
+                                    neighbours.append[lines[y-1][x]]
+                                case self.DOWN:
+                                    pass
 
+    def load(self):
+        with open(str(self.path), 'r') as file:
+            lines = [line.strip() for line in file.readlines()]
+        self._parse(lines)
     
 if __name__ == '__main__':
     tg = TxtGraph('graphs/graph.txt')
