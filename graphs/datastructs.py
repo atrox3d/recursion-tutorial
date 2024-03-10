83cbc14e91d919853795ec7e80ab5102581e1a67
atrox3d <atrox3d@gmail.com>
2024-03-10 12:05:39 +0100

    test Stack and Queue

--- graphs/datastructs.py
+++ graphs/datastructs.py
@@ -11,3 +11,28 @@ class Stack(list):
     ''' adds alias method push to list '''
     def push(self, *args, **kwargs):
         return self.append(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    stack = Stack()
+    queue = Queue()
+
+    print('test stack')
+    for i in range(5):
+        print(f'push {i}')
+        stack.push(i)
+    
+    while len(stack):
+        i = stack.pop()
+        print(f'pop {i}')
+    
+    print()
+
+    print('test queue')
+    for i in range(5):
+        print(f'push {i}')
+        queue.push(i)
+    
+    while len(queue):
+        i = queue.shift()
+        print(f'shift {i}')
