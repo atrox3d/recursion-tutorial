abf14679af089c8f577081c0aee9b202230bd1a4
atrox3d <atrox3d@gmail.com>
2024-03-11 07:16:20 +0100

    cannot use recursion in bfs

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -2,7 +2,9 @@ from datastructs import Queue
 from loaders.jsongraph import JsonGraph
 from loaders.txtgraphs import TxtGraph
 
-def breadthfirst(graph: list[list[str]], source:str, print=lambda *args, **kwargs:None):
+dontprint = lambda *args, **kwargs:None
+
+def breadthfirst(graph: list[list[str]], source:str, print=dontprint):
     stack = Queue([source])                 # start from source
 
     while len(stack):                       # process all stack
@@ -12,6 +14,12 @@ def breadthfirst(graph: list[list[str]], source:str, print=lambda *args, **kwarg
         for neighbour in graph[current]:    # push node neighbours
             stack.push(neighbour)
         
+def rbreadthfirst(graph: list[list[str]], source:str, print=dontprint):
+    ''' cannot use recursion with bfs because the function call STACK '''
+    print(source)
+    for neighbour in graph[source]:
+        rbreadthfirst(graph, neighbour, print)
+
 
 if __name__ == '__main__':
     tg = TxtGraph('graphs/graph.txt')
@@ -20,4 +28,7 @@ if __name__ == '__main__':
     for k, v in graph.items():
         print(f'{k}: {v}')
     
+    print()
     breadthfirst(graph, 'A', print)
+    print()
+    rbreadthfirst(graph, 'A', print)
