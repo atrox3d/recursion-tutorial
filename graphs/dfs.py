ded448fa2f850c0e4f60753e74a4d14b8c95a387
atrox3d <atrox3d@gmail.com>
2024-03-11 06:37:04 +0100

    recursive dfs

--- graphs/dfs.py
+++ graphs/dfs.py
@@ -2,7 +2,9 @@ from datastructs import Stack
 from loaders.jsongraph import JsonGraph
 from loaders.txtgraphs import TxtGraph
 
-def depthfirst(graph: list[list[str]], source:str, print=lambda *args, **kwargs:None):
+dontprint = lambda *args, **kwargs:None
+
+def depthfirst(graph: list[list[str]], source:str, print=dontprint):
     stack = Stack([source])                 # start from source
 
     while len(stack):                       # process all stack
@@ -12,6 +14,16 @@ def depthfirst(graph: list[list[str]], source:str, print=lambda *args, **kwargs:
         for neighbour in graph[current]:    # push node neighbours
             stack.push(neighbour)
         
+def rdepthfirst(graph: list[list[str]], source:str, print=dontprint):
+    ''' 
+    - using implicit function call stack instead of Stack() 
+    - the base case is also implicit: graph[source] != []
+    '''
+    print(source)                             # print current node
+    for neighbour in reversed(graph[source]): # get near nodes, if any
+                                              # reversed simulates stack obj
+        rdepthfirst(graph, neighbour, print)  # recurse on every neighbour
+
 
 if __name__ == '__main__':
     tg = TxtGraph('graphs/graph.txt')
@@ -20,4 +32,7 @@ if __name__ == '__main__':
     for k, v in graph.items():
         print(f'{k}: {v}')
     
+    print()
     depthfirst(graph, 'A', print)
+    print()
+    rdepthfirst(graph, 'A', print)
