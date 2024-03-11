2b0a86f484ee6f5aeb58ae8c908d34e69cb859d7
atrox3d <atrox3d@gmail.com>
2024-03-11 12:11:04 +0100

    add print json

--- graphs/loaders/jsongraph.py
+++ graphs/loaders/jsongraph.py
@@ -3,15 +3,19 @@ from pathlib import Path
 
 
 class JsonGraph:
-    def __init__(self, path, data=None) -> None:
+    def __init__(self, path: str, data: dict=None) -> None:
         self.path = Path(path).absolute()
         self.data = data
 
-    def save(self, indent=2):
+    def save(self, indent=2) -> None:
         with open(str(self.path), 'w') as file:
             json.dump(self.data, file, indent=indent)
 
-    def load(self):
+    def print(self, data=None, indent=None):
+        data = data if data is None else self.data
+        print(json.dumps(data, indent=indent))
+
+    def load(self) -> dict:
         with open(str(self.path), 'r') as file:
             self.data = json.load(file)
-
+        return self.data
